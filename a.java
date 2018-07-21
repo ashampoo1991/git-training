4b2707d95d49e63eb8eaab915eda74c844ca8c14
Ivan Tepliakov <frostfather@mail.ru>
2018-07-21 12:20:21 +0300

    Remove all hello msgs

--- a.java
+++ a.java
@@ -3,9 +3,7 @@ public class A {
 
 public static void main (String [] args){	
 
-System.out.println("Hello, World");
-System.out.println("Hello, World Second");
-System.out.println("Hello from Second Dev");
+
 	}
 	
 }
