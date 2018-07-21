08d927779395850bc32d0d57dc8cb92fff9efd2d
Ivan Tepliakov <frostfather@mail.ru>
2018-07-21 12:28:11 +0300

    Restored Hello msgs

--- a.java
+++ a.java
@@ -3,6 +3,10 @@ public class A {
 
 public static void main (String [] args){	
 
+System.out.println("Hello, World");
+System.out.println("Hello, World Second");
+System.out.println("Hello from Second Dev");
+System.out.println("Hello from First Dev");
 
 	}
 	
